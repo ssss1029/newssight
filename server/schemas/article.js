12ed325234761a2f9fffb0608010a1d5c015ce4a
ssss1029 <sauravkadavath@gmail.com>
2017-06-10 13:01:06 -0700

    added article schema with watson stuff

--- server/schemas/article.js
+++ server/schemas/article.js
@@ -14,7 +14,45 @@ var ArticleSchema = new Schema({
     urlToImage : String,
     publishedAt : Date,
     savedAt : { type: Date, default: Date.now } // Date at which this article was stored into the database
+    
+    watsonEntityResults : [{
+        type : String,
+        text : String, 
+        sentiment : { score : Number },
+        emotion : {}, // Need to fill this in.
+        relevance : Number,
+        disambiguation : {
+            subtype : [],
+            name : String,
+            dbpedia_resource : String
+        },
+        count : Number
+    }],
 
+    watsonKeywordsResults : [{
+        text : String, 
+        sentiment : { score : Number },
+        relevance : Number,
+        emotions : {
+            sadness : Number,
+            joy : Number, 
+            fear : Number, 
+            disgust : Number, 
+            anger : Number
+        }
+    }],
+
+    watsonSentimentResponse : {
+        targets : [{
+            text : String, 
+            score : Number, // From -1.0 to 1.0 
+            label : String // e.g. "neutral"
+        }],
+        document : { // analysis for the entire document
+            score : Number // From -1.0 to 1.0
+            label : String // e.g. negative 
+        }
+    }
 });
 
 var Article = mongoose.model('Article', ArticleSchema);
